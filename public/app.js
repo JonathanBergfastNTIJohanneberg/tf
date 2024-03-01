8c2b4774291b75c6185ee3b2c015220ad68a14fd
JonathanBergfastNTIJohanneberg <jonathan.bergfast@elev.ga.ntig.se>
2024-03-01 14:57:43 +0100

    Lade till en section där mina articles skall vara

--- public/app.js
+++ public/app.js
@@ -6,13 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
           const dropdownContent = this.nextElementSibling;
           const isActive = dropdownContent.classList.contains("active");
 
-          // Toggle the "active" class to open or close the dropdown
           dropdownContent.classList.toggle("active", !isActive);
 
-          event.stopPropagation(); // Prevent event bubbling
+          event.stopPropagation(); 
       });
 
-      // Close the dropdown when clicking outside of it
       document.addEventListener("click", function(event) {
           const dropdownContent = dropdownButton.nextElementSibling;
           if (!dropdownButton.contains(event.target) && !dropdownContent.contains(event.target)) {
